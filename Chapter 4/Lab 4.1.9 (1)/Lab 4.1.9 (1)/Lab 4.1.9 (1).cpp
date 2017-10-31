6701d09e1c2211d2d4f4ebb509930ecdc5227d56
Igor Pylypchuk <pulupigor@ukr.net>
2017-10-31 22:47:04 +0200

    Add 2 labs

--- Chapter 4/Lab 4.1.9 (1)/Lab 4.1.9 (1)/Lab 4.1.9 (1).cpp
+++ Chapter 4/Lab 4.1.9 (1)/Lab 4.1.9 (1)/Lab 4.1.9 (1).cpp
@@ -2,10 +2,24 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+using namespace std;
 
 int main()
 {
+	int course, numberOfGrades, G;
+	int **arr;
+	cout << "Enter course" << endl;
+	cin >> course;
+	arr = new int *[course];
+	for (int r = 0; r < course; r++)
+	{
+		cin >> numberOfGrades;
+		arr[r] = new int[numberOfGrades];
+		for (int c = 0; c <numberOfGrades; c++)
+		cin>>arr[r][c];
+	}
+
     return 0;
 }
 
