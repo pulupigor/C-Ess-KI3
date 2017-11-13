016ac35b7c618b3f52430c1c8e242a77b72c59a2
Igor Pylypchuk <pulupigor@ukr.net>
2017-11-13 22:40:45 +0200

    Add some labs

---
+++ Chapter 5/Lab 5.3.10 (8)/Lab 5.3.10 (8)/Lab 5.3.10 (8).cpp
@@ -0,0 +1,39 @@
+// Lab 5.3.10 (8).cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include<iostream>
+#include <math.h>
+using namespace std;
+class Point2D
+{
+public:  Point2D(double x, double y);
+		 string toString();
+		 double toDouble();
+		 double distanceTo(Point2D that);
+private:
+	double x;
+	double y;
+};
+Point2D::Point2D(double x, double y)
+{
+	this->x = x;
+	this->y = y;
+};
+double Point2D::distanceTo(Point2D that)
+{
+	return sqrt(pow(that.x - this->x,2) + pow(that.y - this->y, 2));
+};
+
+
+int main()
+{
+	double x1,x2, y1,y2;
+	cout << "Enter coordinates first point:" << endl;
+	cin >> x1>>y1;
+	cout << "Enter coordinates second point:" << endl;
+	cin >> x2>>y2;
+	Point2D firstpoint(x1, y1),secondpoint(x2,y2);
+	cout << firstpoint.distanceTo(secondpoint)<<endl;
+    return 0;
+}
+
