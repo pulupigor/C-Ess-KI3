a52650ce838cf258d9aba50bd4621958dd503ac0
Igor Pylypchuk <pulupigor@ukr.net>
2017-12-01 21:42:45 +0200

    Ended Chapter 7

---
+++ Chapter 7/Lab 7.3.8(1)/Lab 7.3.8(1)/Lab 7.3.8(1).cpp
@@ -0,0 +1,38 @@
+// Lab 7.3.8(1).cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class IPAddress
+{
+private:
+	int range;
+	string address;
+
+public:
+	IPAddress(string address, int range);
+};
+
+IPAddress::IPAddress(string address, int range)
+{
+	int octetcsCount = 0;
+	int index = -1;
+
+	while (address.find(address, index + 1) != -1)
+		octetcsCount++;
+
+	octetcsCount++;
+
+	if (octetcsCount != 4)
+		throw;
+}
+
+
+int main()
+{
+  return 0;
+}
+
